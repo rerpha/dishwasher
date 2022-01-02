958c319e54c98b22455389abb733cc072876d3a9
Jack Harper <harps912@gmail.com>
2022-01-02 01:33:25 +0000

    h3

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def hello():
                 <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧽</text></svg>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">            </head>
             <body>
-                <h2>Dishwasher is <button class="btn-large" onClick="change()"> {"loading" if s.loading else "unloading"}</button></h1>
+                <h3>Dishwasher is <button class="btn-large" onClick="change()"> {"loading" if s.loading else "unloading"}</button></h1>
                 <script>
                     function change() {{ 
                         fetch("./change").then(function(response) {{
