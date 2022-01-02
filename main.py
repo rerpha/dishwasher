e542f38e4af064d193520f7a8b3d7c388b712f3e
Jack Harper <harps912@gmail.com>
2022-01-02 01:35:35 +0000

    tidying

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ def hello():
                 <meta name="viewport" content="width=device-width, initial-scale=1">
                 <title>Dishwasher Status</title>
                 <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧽</text></svg>">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">            </head>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+            </head>
             <body>
                 <h3>Dishwasher is <button class="btn-large" onClick="change()"> {"loading" if s.loading else "unloading"}</button></h1>
                 <script>
